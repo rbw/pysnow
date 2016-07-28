5d28cfc15e687db1db10421fd47755eaf698110f
Robert Wikman <rbw@vault13.org>
2016-07-28 17:58:33 +0200

    Added sphinx documentation

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:50,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to pysnow&#8217;s documentation!"],titleterms:{document:0,indice:0,pysnow:0,tabl:0,welcom:0}})
