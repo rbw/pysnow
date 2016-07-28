734b002cae4af4d4940af9996077d3207a36ce23
Robert Wikman <rbw@vault13.org>
2016-07-29 00:43:21 +0200

    Added pypi configuration

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from pysnow import __version__
+
+setup(
+    name='pysnow',
+    packages=['pysnow'],
+    version=__version__,
+    description='Python library for the ServiceNow REST API',
+    install_requires=['requests'],
+    author='Robert Wikman',
+    author_email='rbw@vault13.org',
+    maintainer='Robert Wikman',
+    maintainer_email='rbw@vault13.org',
+    url='https://github.com/rbw0/pysnow',
+    download_url='https://github.com/rbw0/pysnow/tarball/%d' % __version__,
+    keywords=['servicenow', 'rest', 'api', 'http'],
+    classifiers=[],
+    license='GPLv2',
+)
