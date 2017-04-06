da3759c6bdfe7d11bc92c81b2a4dd37668fcf930
Robert Wikman <rbw@vault13.org>
2017-04-06 13:40:44 +0200

    Fixed import issue

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
-#import sys
-#sys.path.insert(0, os.path.abspath('..'))
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 import pysnow
 
 master_doc = "index"
