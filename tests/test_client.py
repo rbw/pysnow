3b1f3a4a36ad386e844159e9598eccdfdc469a8d
Robert Wikman <rbw@vault13.org>
2017-04-14 22:17:27 +0200

    2.6 compatibility fixes

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,27 +2,20 @@
 import unittest
 import requests
 
-import pysnow
-
 from pysnow.client import Client
 from pysnow.exceptions import InvalidUsage
+
 
 class TestClient(unittest.TestCase):
     def test_client_missing_args(self):
         """Client should raise an exception if it is missing arguments."""
-        with self.assertRaises(InvalidUsage):
-            Client("snow.example.com")
-
-        with self.assertRaises(InvalidUsage):
-            Client("snow.example.com", user="foo")
-
-        with self.assertRaises(InvalidUsage):
-            Client("snow.example.com", password="foo")
+        self.assertRaises(InvalidUsage, Client, instance="test")
+        self.assertRaises(InvalidUsage, Client, instance="test", user="foo")
+        self.assertRaises(InvalidUsage, Client, instance="test", password="foo")
 
     def test_client_incompabtible_args(self):
         """Client should raise an exception if it receives incompatible args."""
-        with self.assertRaises(InvalidUsage):
-            Client("snow.example.com", user="foo", password="bar", session="foobar")
+        self.assertRaises(InvalidUsage, Client, instance="test", user="foo", password="bar", session="foobar")
 
     def test_client_user_password(self):
         """Should be able to create a client given user name and password."""
