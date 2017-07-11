049883efc7bfcfc56f8adee93550935ffd2df2eb
Robert Wikman <rbw@vault13.org>
2017-07-11 17:44:07 +0200

    Doc fixes

--- pysnow/client.py
+++ pysnow/client.py
@@ -21,6 +21,7 @@ class Client(object):
         :param user: username
         :param password: password
         :param raise_on_empty: whether or not to raise an exception on 404 (no matching records)
+        :param default_payload: deprecated, use request_params
         :param request_params: request params to send with requests
         :param session: a requests session object
         """
