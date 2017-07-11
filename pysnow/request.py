3b67d78800d810c46f820dc3f930b23256cfe776
Robert Wikman <rbw@vault13.org>
2017-07-11 15:21:42 +0200

    result => params

--- pysnow/request.py
+++ pysnow/request.py
@@ -338,16 +338,16 @@ class Request(object):
         else:
             raise InvalidUsage("Query must be instance of %s, %s or %s" % (query.QueryBuilder, str, dict))
 
-        result = {'sysparm_query': sysparm_query}
-        result.update(self.request_params)
+        params = {'sysparm_query': sysparm_query}
+        params.update(self.request_params)
 
         if limit is not None:
-            result.update({'sysparm_limit': limit, 'sysparm_suppress_pagination_header': True})
+            params.update({'sysparm_limit': limit, 'sysparm_suppress_pagination_header': True})
 
         if len(fields) > 0:
             if isinstance(fields, list):
-                result.update({'sysparm_fields': ",".join(fields)})
+                params.update({'sysparm_fields': ",".join(fields)})
             else:
                 raise InvalidUsage("You must pass the fields as a list")
 
-        return result
+        return params
