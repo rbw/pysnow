38ff1948020dbd82e9e4cb86bd3f41993b6e7461
rbw0 <rbw@vault13.org>
2017-11-28 21:08:48 +0100

    Added documentation

--- pysnow/report.py
+++ pysnow/report.py
@@ -3,6 +3,13 @@
 
 class Report(object):
     def __init__(self, resource, request_params, generator_size, session):
+        """Constructs a report, keeping track of resources, requests and responses
+
+        :param resource: :class:`pysnow.Resource <Resource>` object
+        :param request_params: Dictionary of request params
+        :param generator_size: Generator size (integer)
+        :param session: :class:`requests.Session <Session>` object
+        """
         self.x_total_count = -1
         self.consumed_records = 0
         self.responses = []
@@ -12,12 +19,24 @@ class Report(object):
         self.session = session
 
     def set_x_total_count(self, count):
+        """Sets the x-total-count (from response header)
+
+        :param count: total count
+        """
         self.x_total_count = int(count)
 
     def add_consumed_count(self, records):
+        """Adds the number of records present in a response to the :prop:`consumed_count`
+
+        :param records: response content record count
+        """
         self.consumed_records = self.consumed_records + int(records)
 
     def add_response(self, response):
+        """Adds a :class:`requests.Response <Response>` object to the :prop:`responses` list
+
+        :param response: :class:`requests.Response <Response>` object
+        """
         self.responses.append(response)
 
     def __getitem__(self, key):
