71eec895deb660f6fd7e8cd4cc56505adfe18af9
Robert Wikman <rbw@vault13.org>
2017-12-05 00:15:33 +0100

    JSON parser fixes

--- pysnow/response.py
+++ pysnow/response.py
@@ -3,120 +3,165 @@
 import ijson.backends.yajl2_cffi as ijson
 from ijson.common import ObjectBuilder
 
+from itertools import chain
+
 from requests.exceptions import HTTPError
 
 from .exceptions import (ResponseError,
                          NoResults,
                          MultipleResults,
-                         UnexpectedResponseFormat,
                          MissingResult)
 
 
 class Response(object):
-    """Takes a :class:`requests.Response` object and performs deserialization and validation and offers
-    an interface for obtaining the content in a safe manner.
+    """Takes a :class:`requests.Response` object and performs deserialization and validation.
 
     :param response: :class:`request.Response` object
     :param raise_on_empty: whether or not to raise an exception if the content doesn't contain any records
+    :param chunk_size: Read and return up to this size (in bytes) in the stream parser
     """
 
-    def __init__(self, response, raise_on_empty):
+    def __init__(self, response, raise_on_empty, chunk_size=1024):
         self._raise_on_empty = raise_on_empty
         self._response = response
-        self.count = 0
+        self._chunk_size = chunk_size
+        self._count = 0
 
-    def _get_validated_content(self, response):
-        """Validates the content after performing deserialization of the JSON body
+    @property
+    def count(self):
+        return self._count
 
-        :param response: :class:`requests.Response` object
-        :return: validated content as list, of records if any.
-        :raise:
-            :ResponseError: If `error` was present in the content
-            :MissingResult: If no `result` was present in the content
-            :NoResults: If no records and raise_on_empty is set to True
-        """
+    @count.setter
+    def count(self, count):
+        if not isinstance(count, int) or isinstance(count, bool):
+            raise TypeError("Count must be an integer")
 
-        # Don't even attempt to deserialize successful delete, as it doesn't contain a body.
-        if response.request.method == 'DELETE' and response.status_code == 204:
-            return [{'status': 'record deleted'}]
-
-        if 'error' in content:
-            raise ResponseError(content['error'])
-
-        # In rare cases `result` may not be present in the response content. Let the user know.
-        if 'result' not in content:
-            raise MissingResult('The expected `result` key was missing in the response from ServiceNow. '
-                                'Cannot continue')
-
-        try:
-            # Raise an HTTPError if we hit a non-200 status code
-            response.raise_for_status()
-        except HTTPError:
-            # Versions prior to Helsinki returns 404 on empty result sets
-            if response.status_code == 404:
-                if self._raise_on_empty is True:
-                    raise NoResults('Query yielded no results')
-                else:
-                    return [{}]
-            raise
-
-        # Helsinki and later returns status 200 instead of 404 on empty result sets
-        if len(content['result']) < 1:
-            if self._raise_on_empty is True:
-                raise NoResults('Query yielded no results')
-
-            return [{}]
-
-        return content['result']
-
-    def add_record_count(self, count):
-        """Updates the record counter
-
-        :param count: Count to be added
-        """
-        #self.add_record_count(len(content['result']))
-        self.count = self.count + int(count)
+        self._count = count
 
     def __repr__(self):
         return '<%s [%d - %s]>' % (self.__class__.__name__, self._response.status_code, self._response.request.method)
 
+    def _parse_response(self):
+        """Looks for `result.item` (array), `result` (object) and `error` (object) keys and parses
+        the raw response content (stream of bytes)
+
+        :raise:
+            :ResponseError: If there's an error in the response
+            :NoResults: If empty result set and raise_on_empty is set to True
+            :MissingResult: If no result nor error was found
+        """
+
+        has_single_result = False
+        has_many_result = False
+        has_error = False
+
+        for prefix, event, value in ijson.parse(self._response.raw, buf_size=self._chunk_size):
+            if (prefix, event) == ('error', 'start_map'):
+                # We've found a ServiceNow `error` object at the root
+                has_error = True
+                builder = ObjectBuilder()
+            elif (prefix, event) == ('result', 'start_map'):
+                # We've found a ServiceNow `result` object at the root
+                has_single_result = True
+                builder = ObjectBuilder()
+            elif (prefix, event) == ('result', 'start_array'):
+                # We've found a ServiceNow `result` array at the root
+                has_many_result = True
+                builder = ObjectBuilder()
+
+            if has_many_result:
+                # Build the result
+                if (prefix, event) == ('result.item', 'end_map'):
+                    # Reached end of object. Set count and yield
+                    builder.event(event, value)
+                    self.count += 1
+                    yield getattr(builder, 'value')
+                elif prefix.startswith('result.item'):
+                    # Build the result object
+                    builder.event(event, value)
+            elif has_single_result:
+                if (prefix, event) == ('result', 'end_map'):
+                    # Reached end of the result object. Set count and yield.
+                    builder.event(event, value)
+                    self.count += 1
+                    yield getattr(builder, 'value')
+                elif prefix.startswith('result'):
+                    # Build the error object
+                    builder.event(event, value)
+            elif has_error:
+                if (prefix, event) == ('error', 'end_map'):
+                    # Reached end of the error object - raise ResponseError exception
+                    raise ResponseError(getattr(builder, 'value'))
+                elif prefix.startswith('error'):
+                    # Build the error object
+                    builder.event(event, value)
+
+        if (has_single_result or has_many_result) and self.count == 0:  # Results empty
+            if self._raise_on_empty is True:
+                # Raise exception if it was requested
+                raise NoResults('Query yielded no results')
+
+            # Otherwise just yield empty dict
+            yield {}
+
+        if not (has_single_result or has_many_result or has_error):  # None of the expected keys were found
+            raise MissingResult('The expected `result` key was missing in the response. Cannot continue')
+
+    def _get_validated_response(self):
+        """Validates response then calls :meth:`_parse_response` to yield content
+
+        Immediately yields response content if request method is DELETE and code 204 (this response never
+        contains a body).
+
+        :raise:
+            :NoResults: On status 404 if raise_on_empty is set to True
+        """
+
+        response = self._response
+
+        if response.request.method == 'DELETE' and response.status_code == 204:
+            yield [{'status': 'record deleted'}]
+        else:
+            try:
+                # Raise an HTTPError if we hit a non-200 status code
+                response.raise_for_status()
+            except HTTPError as e:
+                # Versions prior to Helsinki returns 404 on empty result sets
+                if response.status_code == 404:
+                    if self._raise_on_empty is True:
+                        raise NoResults('Query yielded no results')
+                    else:
+                        yield [{}]
+                else:
+                    raise e
+
+            # Parse byte stream
+            yield self._parse_response()
+
     def all(self):
-        """Yields streamed response
+        """Returns a chained generator response containing all matching records
 
         :return: Iterable response
         """
 
-        key = '-'
-        for prefix, event, value in ijson.parse(self._response.raw):
-            if prefix == '' and event == 'map_key':  # found new object at the root
-                key = value  # mark the key value
-                builder = ObjectBuilder()
-            elif key == 'error' and event == 'end_map':
-                raise ResponseError(getattr(builder, 'value'))
-            elif prefix.startswith(key):  # while at this key, build the object
-                builder.event(event, value)
-                if event == 'end_map':  # found the end of an object at the current key, yield
-                    yield getattr(builder, 'value')
+        return chain.from_iterable(self._get_validated_response())
 
     def first(self):
-        """Returns the first item in the response content after deserialization and validation
+        """Return the first record or raise an exception if the result doesn't contain any data
 
         :return: Dictionary containing the first item in the response content
         :raise:
             :NoResults: If no results were found
         """
 
-        content = self._get_validated_content(self._response)
+        self._raise_on_empty = True
 
-        if len(content) >= 1:
-            return content[0]
-
-        raise NoResults
+        return next(self.all())
 
     def first_or_none(self):
-        """Returns the first item in the response content or None
+        """Return the first record or None
 
-        :return: Dictionary containing the first item in the response content, or None if no records
+        :return: Dictionary containing the first item or None
         """
 
         try:
@@ -125,7 +170,7 @@ class Response(object):
             return None
 
     def one(self):
-        """Returns a dictionary if the content contains exactly one item
+        """Return exactly one result or raise an exception.
 
         :return: Dictionary containing the only item in the response content
         :raise:
@@ -133,19 +178,24 @@ class Response(object):
             :NoResults: If no records are present in the content
         """
 
-        content = self._get_validated_content(self._response)
+        self._raise_on_empty = True
 
-        if len(content) > 1:
+        r = self.all()
+        result = next(r)
+
+        try:
+            next(r)
+        except StopIteration:
+            pass
+        else:
             raise MultipleResults("Expected single-record result, got multiple")
-        elif len(content) < 1:
-            raise NoResults("Expected single-record result, got none")
 
-        return content[0]
+        return result
 
     def one_or_none(self):
-        """Returns a dictionary if the content contains exactly one item, or None
+        """Return at most one result or raise an exception.
 
-        :return: Dictionary containing the only item in the response content, or None
+        :return: Dictionary containing the matching record or None
         """
 
         try:
