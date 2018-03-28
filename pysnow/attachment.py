545e78d178c520cda45991ce02c22c1a05abd7ca
rbw0 <rbw@vault13.org>
2018-03-28 22:53:00 +0200

    Added docstrings

--- pysnow/attachment.py
+++ pysnow/attachment.py
@@ -1,21 +1,46 @@
 # -*- coding: utf-8 -*-
 
 import os
+from pysnow.exceptions import InvalidUsage
 
 
 class Attachment(object):
-    def __init__(self, resource, api_name, resource_name):
-        self.resource = resource
-        self.api_name = api_name
-        self.resource_name = resource_name
+    """API for managing attachments.
 
-    def get(self, sys_id=None, limit=10):
+    :param resource: API resource to manage attachments for
+    :param table_name: Name of the table to use in the attachment API
+    """
+
+    def __init__(self, resource, table_name):
+        self.resource = resource
+        self.table_name = table_name
+
+    def get(self, sys_id=None, limit=100):
+        """Returns a list of records,
+
+        :param sys_id: record sys_id to list attachments for
+        :param limit: override the default limit of 100
+        :return: list of incident objects
+        """
+
         if sys_id:
-            return self.resource.get(query={'sys_id': sys_id}).one()
+            return self.resource.get(query={'table_sys_id': sys_id, 'table_name': self.resource_name}).all()
 
         return self.resource.get(query={'table_name': self.resource_name}, limit=limit).all()
 
     def upload(self, sys_id, file_path, name=None, multipart=False):
+        """Attaches a new file to the specified record
+
+        :param sys_id: the sys_id of the record to attach the file to
+        :param file_path: local absolute path of the file to upload
+        :param name: custom name for the uploaded file (instead of basename)
+        :param multipart: whether or not to use multipart
+        :return: the inserted record
+        """
+
+        if not isinstance(multipart, bool):
+            raise InvalidUsage('Multipart must be of type bool')
+
         resource = self.resource
 
         if name is None:
@@ -27,9 +52,7 @@ class Attachment(object):
             'file_name': name
         })
 
-        # Set the payload
         data = open(file_path, 'rb').read()
-
         headers = {}
 
         if multipart:
@@ -39,8 +62,14 @@ class Attachment(object):
             headers["Content-Type"] = "text/plain"
             path_append = '/file'
 
-        return resource.request(method='POST', data=data, headers=headers, path_append=path_append)
+        return resource.request(method='POST', data=data, headers=headers, path_append=path_append).one()
 
     def delete(self, sys_id):
+        """Deletes the specified record
+
+        :param sys_id: attachment sys_id
+        :return: delete result
+        """
+
         self.resource.delete(query={'sys_id': sys_id})
 
