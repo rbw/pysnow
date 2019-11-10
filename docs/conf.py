0d38a85b1205c82df0e7c1b86c53239d50716db6
Robert Wikman <rbw@vault13.org>
2019-11-10 16:20:13 +0100

    Sphinx conf cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -2,18 +2,15 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
-import pysnow
-import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 master_doc = "index"
 
 project = "pysnow"
 
-version = release = pysnow.__version__
 
 extensions = [
     "sphinx.ext.autodoc",
